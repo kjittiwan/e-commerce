1b45639fff88e568a79eeb73ada45e1c45d11917
Kann Jittiwan <kjittiwan2@gmail.com>
2023-05-03 21:09:57 +0700

    Added hero section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,35 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { SidebarContext } from "../contexts/SidebarContext";
 import { BsCart3 } from 'react-icons/bs'
 import { CartContext } from "../contexts/CartContext";
 import { Link } from "../../node_modules/react-router-dom/dist/index";
 import Logo from '../images/logo.png'
 function Header() {
+  const [isSticky, setIsSticky] = useState(false)
   const { setIsOpen } = useContext(SidebarContext)
   const { cartAmount } = useContext(CartContext)
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      console.log(window.scrollY)
+      window.scrollY > 800 ? setIsSticky(true) : setIsSticky(false)
+    })
+  })
   return (
-    <div className=" flex justify-between items-center px-6 py-6 border-b drop-shadow-lg">
-      <Link to={'/'} className='flex text-xl md:text-3xl gap-x-2 hover:scale-110 active:translate-y-1 drop-shadow-sm items-end'>
-        <img src={Logo} alt="logo" className=" h-8  md:h-10" />
-        <div className="">
-          Fake
-        </div>
+    <header className={` ${isSticky ? 'fixed mt-0 drop-shadow-lg' : 'absolute mt-10'} bg-white  w-full z-10 transition-all duration-500 flex justify-between items-center px-6    py-5 `}>
+      <Link to={'/'} className='flex text-2xl font-light  gap-x-2 hover:scale-110 active:translate-y-1 drop-shadow-sm items-end'>
+      CHLOE
       </Link>
       <div className="flex items-center gap-x-5">
         {/* <div className="text-md font-semibold uppercase">Products</div> */}
-        <div onClick={() => setIsOpen(true)} className=' text-3xl md:text-4xl relative hover:scale-110 transition active:translate-y-1'>
+        <div onClick={() => setIsOpen(true)} className=' text-3xl relative hover:scale-110 transition active:translate-y-1'>
           <BsCart3 />
-          <div className={`${cartAmount === 0 ? 'opacity-0' : 'opacity-100'} bg-[#FF0100] flex justify-center items-center w-4 h-4 md:w-5 md:h-5 rounded-full text-xs text-white absolute -top-1.5 md:-top-2 -right-2`}>{cartAmount}</div>  
+          <div className={`${cartAmount === 0 ? 'opacity-0' : 'opacity-100'} bg-[#FF0100] flex justify-center items-center w-4 h-4  rounded-full text-xs text-white absolute -top-1.5  -right-2`}>{cartAmount}</div>  
         </div>
        
         
       </div>
       
-    </div>
+    </header>
   );
 }
 
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,28 @@
+import heroImg from '../images/hero.jpg'
+import { Link } from '../../node_modules/react-router-dom/dist/index';
+import { IoMdArrowForward } from "react-icons/io"
 function Hero() {
   return (
-    <div>
-      HERO
-    </div>
+    <section className=" bg-[#fcf2e9]  h-[100vh] flex flex-col justify-between ">
+      <div className='uppercase flex justify-center items-center py-2 mb-16'>Free shipping on orders of $50+</div>
+      <div className='h-full  flex items-end'>
+        <div style={{backgroundImage: `url(${heroImg})`}} className='h-[100%] w-[45%] bg-cover'>
+        </div>
+        <div className='flex flex-col px-28 justify-start h-full w-[55%]  pt-32'>
+          <div>
+            <div className='flex items-center font-semibold uppercase justify-start'>
+              <div className='w-10 h-[2px] bg-red-600 mr-3'></div>Why wait ?
+            </div>
+            <div className=' uppercase font-light text-[65px] '>Discover <br/>your unique <br/><span className='font-bold'>Style</span></div>
+            <Link to={'/'} className='uppercase font-semibold  hover:scale-105 flex items-center w-fit border-b-2 border-black'>
+              <div className=''>Discover more</div>
+              <IoMdArrowForward className='ml-2' />
+            </Link>        
+          </div>
+         
+        </div>
+      </div>
+    </section>
   );
 }
 
