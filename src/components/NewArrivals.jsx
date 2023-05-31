ebfd362bf0e56f9b9a2e64726a7e78c69be57a87
Kann Jittiwan <kjittiwan2@gmail.com>
2023-05-31 22:31:00 +0700

    additional edits

--- src/components/NewArrivals.jsx
+++ src/components/NewArrivals.jsx
@@ -9,7 +9,7 @@ function NewArrivals() {
           </div>
           <div className="flex w-[80%] md:w-[60%] h-[400px] gap-x-2 drop-shadow-lg">
             <div className=" bg-coat bg-cover  w-[45%] relative group">
-              <Link to={'/product/17'} className="bg-black bg-opacity-60 absolute bottom-0 h-[60px] w-full text-white text-sm md:text-xl font-light flex items-center justify-between px-4 opacity-0 group-hover:opacity-100 transition-all duration-300">
+              <Link to={'/product/17'} className="bg-black bg-opacity-60 absolute bottom-0 h-[60px] w-full text-white text-sm md:text-xl font-light flex items-center justify-between px-2 md:px-4 opacity-0 group-hover:opacity-100 transition-all duration-300">
                 <div>$ 39.99</div>
                 <div className="flex items-center gap-x-2">
                   Buy now
@@ -19,7 +19,7 @@ function NewArrivals() {
             </div>
             <div className="flex flex-col flex-1 gap-y-2 ">
                 <div className="bg-bag bg-cover bg-center flex-1 relative group ">
-                <Link to={'/product/1'} className="bg-black bg-opacity-60 absolute bottom-0 h-[60px] w-full text-white text-md md:text-xl font-light flex items-center justify-between px-4 opacity-0 group-hover:opacity-100 transition-all duration-300">
+                <Link to={'/product/1'} className="bg-black bg-opacity-60 absolute bottom-0 h-[60px] w-full text-white text-sm md:text-xl font-light flex items-center justify-between px-2 md:px-4 opacity-0 group-hover:opacity-100 transition-all duration-300">
                   <div>$ 39.99</div>
                   <div className="flex items-center gap-x-2">
                     Buy now
@@ -28,7 +28,7 @@ function NewArrivals() {
                 </Link>
               </div>
               <div className="bg-jacket bg-cover bg-center flex-1 relative group">
-                <Link to={'/product/16'} className="bg-black bg-opacity-60 absolute bottom-0 h-[60px] w-full text-white text-md md:text-xl font-light flex items-center justify-between px-4 opacity-0 group-hover:opacity-100 transition-all duration-300">
+                <Link to={'/product/16'} className="bg-black bg-opacity-60 absolute bottom-0 h-[60px] w-full text-white text-sm md:text-xl font-light flex items-center justify-between px-2 md:px-4 opacity-0 group-hover:opacity-100 transition-all duration-300">
                   <div className=''>$ 39.99</div>
                   <div className="flex items-center gap-x-2">
                     Buy now
